89b68b5175ab2bb29bd402e4d4358e153c4d929f
ShaneEverittM <shaneemurphy146@gmail.com>
2020-07-28 02:23:56 -0700

    some progress

--- __main__.py
+++ __main__.py
@@ -4,13 +4,29 @@ import tkinter as tk
 
 def main():
     root = tk.Tk()
-    frame = tk.Frame(root)
+    frame = tk.Frame(root, width=100, height=100)
     frame.pack()
-    m = Maze(frame, 100, 100)
-    m.add_wall((50, 50), m.Direction.Left)
-    m.add_wall((50, 50), m.Direction.Right)
-    m.add_wall((50, 50), m.Direction.Up)
-    m.add_wall((50, 50), m.Direction.Down)
+    root.update()
+    print("frame height: " + str(frame.winfo_height()))
+    print("frame width: " + str(frame.winfo_width()))
+    m = Maze(frame)
+    for x in range(1, 50):
+        for y in range(1, 50):
+            m.add_square((x, y))
+    root.update()
+    print("frame height: " + str(frame.winfo_height()))
+    print("frame width: " + str(frame.winfo_width()))
+
+    # m.add_wall((5, 5), m.Direction.Left)
+    # m.add_wall((5, 5), m.Direction.Right)
+    # m.add_wall((5, 5), m.Direction.Up)
+    # m.add_wall((5, 5), m.Direction.Down)
+
+    # m.add_wall((15, 5), m.Direction.Right)
+    # m.add_wall((15, 5), m.Direction.Up)
+    # m.add_wall((15, 5), m.Direction.Down)
+    # m.add_wall((15, 5), m.Direction.Left)
+
     root.mainloop()
 
 
--- maze.py
+++ maze.py
@@ -4,7 +4,7 @@ import tkinter.messagebox as tkMessageBox
 import operator
 
 MAZE_ROOT = 10, 10
-PIXELS_PER_SQUARE = 10
+PIXELS_PER_SQUARE = 20
 
 
 # Given a tkinter frame, will attempt to generate and render a maze within it.
@@ -15,12 +15,12 @@ class Maze:
         Up = 3
         Down = 4
 
-    def __init__(self, frame, height_in_squares, width_in_squares):
+    def __init__(self, frame: tk.Frame, height_in_squares, width_in_squares):
         self.canvas = tk.Canvas(frame, bg="grey", height=1000, width=1000)
         self.button = tk.Button(frame, text="Popup",
                                 command=Maze.popupCallback)
-        self.height = height_in_squares * PIXELS_PER_SQUARE
-        self.width = width_in_squares * PIXELS_PER_SQUARE
+        self.height = frame.winfo_height() * PIXELS_PER_SQUARE * height_in_squares
+        self.width = frame.winfo_width() * PIXELS_PER_SQUARE * width_in_squares
         self.canvas.pack()
         self.button.pack()
 
@@ -43,20 +43,25 @@ class Maze:
             print("Error")
             return
 
-        wall_start = Maze.tuple_add(
-            center_coords, Maze.tuple_mul(PIXELS_PER_SQUARE, start_offset))
+        scaled_center_coords = Maze.tuple_mul(PIXELS_PER_SQUARE, center_coords)
+        wall_start = Maze.tuple_add(scaled_center_coords,
+                                    Maze.tuple_mul(PIXELS_PER_SQUARE/2, start_offset))
         wall_end = Maze.tuple_add(
-            center_coords, Maze.tuple_mul(PIXELS_PER_SQUARE, end_offset))
+            scaled_center_coords, Maze.tuple_mul(PIXELS_PER_SQUARE/2, end_offset))
         self.canvas.create_line(wall_start + wall_end, width=2)
 
-    @staticmethod
+    def add_square(self, center_coords):
+        for direction in Maze.Direction:
+            self.add_wall(center_coords, direction)
+
+    @ staticmethod
     def tuple_add(t1, t2):
         return tuple(item1 + item2 for item1, item2 in zip(t1, t2))
 
-    @staticmethod
+    @ staticmethod
     def tuple_mul(scalar, tup):
         return tuple(scalar * item for item in tup)
 
-    @staticmethod
+    @ staticmethod
     def popupCallback():
         tkMessageBox.showinfo("Name", "Popup!")
